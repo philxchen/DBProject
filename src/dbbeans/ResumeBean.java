501ba8354d79269d01326ba73325949b4170b85c
philxchen <cxuankai@outlook.com>
2017-03-25 16:33:04 -0400

    updated ResumeBean

--- src/dbbeans/ResumeBean.java
+++ src/dbbeans/ResumeBean.java
@@ -1,5 +1,7 @@
 package dbbeans;
+
 import java.sql.*;
+
 /**
  * Created by michaelhuang on 2017-03-25.
  */
@@ -10,24 +12,21 @@ public class ResumeBean {
     private ResultSet rs;
 
 
-
-    public void setDataAccess(DataAccess db)
-    {
+    public void setDataAccess(DataAccess db) {
         dataaccess = db;
     }
 
 
-    public void insertResume(int userID,int version_num, int mark , DataAccess db)
-    {
+    public void insertResume(int userID, int versionNum, String education, String skill, String workExp, DataAccess db) {
         connection = db.getConnection();
         try {
             st = connection.createStatement();
-            st.executeUpdate("INSERT INTO Jobs "
-                    + " VALUES ("+userID+","+ companyName+","+mark + ")");
+            st.executeUpdate("INSERT INTO Resume "
+                    + " VALUES (" + userID + "," + versionNum + "," + education + "," + skill + ", " + workExp + ")");
             rs.close();
             st.close();
-        }catch(Exception e){
-            System.out.println("Cant insert into Jobs");
+        } catch (Exception e) {
+            System.out.println("Cant insert into Resume");
         }
     }
 
