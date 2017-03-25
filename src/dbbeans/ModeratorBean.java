c87bcbc06bc2ea0089ccae095559ec9ad95e666b
michael huang <jinyanghuangjj@gmail.com>
2017-03-25 14:44:10 -0400

    implemented moderatorbeans

--- src/dbbeans/ModeratorBean.java
+++ src/dbbeans/ModeratorBean.java
@@ -1,7 +1,41 @@
 package dbbeans;
-
+import java.sql.*;
 /**
  * Created by philxchen on 3/25/17.
  */
 public class ModeratorBean {
+    private Connection connection;
+    private Statement st;
+    private DataAccess dataaccess;
+    private ResultSet rs;
+    private String getStudentList="";
+
+
+
+    public void setDataAccess(DataAccess db)
+    {
+        dataaccess = db;
+    }
+
+
+    public void insertModerator(int userID, DataAccess db)
+    {
+        connection = db.getConnection();
+        try {
+            st = connection.createStatement();
+            st.executeUpdate("INSERT INTO student "
+                    + " VALUES ("+userID+",Default )");
+            rs.close();
+            st.close();
+        }catch(Exception e){
+            System.out.println("Cant insert into Users");
+        }
+    }
+
+
+
+
 }
+
+
+
